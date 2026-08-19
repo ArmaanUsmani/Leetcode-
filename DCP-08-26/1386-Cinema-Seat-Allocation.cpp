9fbaf2430b81ef5368b4a55dc01f1b0e98c1456f
ArmaanUsmani <137693517+ArmaanUsmani@users.noreply.github.com>
2026-08-19 19:26:14 +0530

    [1386] [Time Beats: 95.27%] [Memory Beats: 82.09%] - LeetPush

--- DCP-08-26/1386-Cinema-Seat-Allocation.cpp
+++ DCP-08-26/1386-Cinema-Seat-Allocation.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        int ans=n*2;
-        sort(reservedSeats.begin(),reservedSeats.end());
-        for(int i=0;i<reservedSeats.size();i++){
-            int curr=reservedSeats[i][0];
-            int flag25=1;
-            int flag47=1;
-            int flag69=1;
-            while(i<reservedSeats.size()&&reservedSeats[i][0]==curr){
-                if(reservedSeats[i][1]>=2&&reservedSeats[i][1]<=5){
-                    flag25=0;
-                    if(reservedSeats[i][1]>=4&&reservedSeats[i][1]<=5){
-                        flag47=0;
-                    }
-                }else if(reservedSeats[i][1]>=6&&reservedSeats[i][1]<=9){
-                    flag69=0;
-                    if(reservedSeats[i][1]>=6&&reservedSeats[i][1]<=7){
-                        flag47=0;
-                    }
-                }
-                i++;
-
+        int l=0b11110000;
+        int m=0b11000011;
+        int r=0b00001111;
+        unordered_map<int,int> mp;
+        for(auto &i:reservedSeats) {
+            if(i[1]>=2 && i[1]<=9) {
+                mp[i[0]]|=(1<<(i[1]-2));
             }
-           if(flag69==0&&flag25==0&&flag47==0){
-            ans--;
-            ans--;
-           }else if(flag69==0||flag25==0||flag47==0){
-            ans--;
-           }
-           i--;
-           
+        }
+        int ans=2*(n-mp.size());
+        for(auto &i:mp) {
+            if(((i.second|l)==l) || ((i.second|m)==m) || ((i.second|r)==r)) {
+                ans++;
+            }
         }
         return ans;
     }
